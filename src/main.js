e8bb48eb6a6c497540f69081e2ad027e8375ce26
ishizuka takeaki <>
2021-01-23 23:24:28 +0900

    add sass

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue  from 'bootstrap-vue'
+
+require('@/assets/sass/app.scss')
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
